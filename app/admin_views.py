9193aada97d93e58e5dc8c4330d3788a1a8b62e9
hraza77420 <hraza77420@gmail.com>
2021-01-03 16:02:57 +0100

    jinja for block

--- app/admin_views.py
+++ app/admin_views.py
@@ -1,5 +1,12 @@
 from app import app
+from flask import render_template
+
 
 @app.route("/admin/dashboard")
 def admin_dashboard():
-    return "Admin dashboard"
+    return render_template("admin/dashboard.html")
+
+@app.route("/admin/profile")
+def admin_profile():
+    return "Profile file"
+
