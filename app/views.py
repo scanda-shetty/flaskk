93283d3ef314fb9914bd2f324f47019bf979325a
hraza77420 <hraza77420@gmail.com>
2021-01-05 16:12:52 +0100

    ep8

--- app/views.py
+++ app/views.py
@@ -1,5 +1,6 @@
 from app import app 
 from flask import render_template
+from flask import request, redirect
 
 @app.route("/")
 def index():
@@ -63,3 +64,44 @@ def jinja():
         friends=friends, colors=colors, cool=cool, GitRemote=GitRemote, 
         my_remote=my_remote, repeat=repeat
     )
+
+@app.route("/sign-up", methods=["GET", "POST"])
+def sign_up():
+    if request.method == "POST":
+        req = request.form
+        missing = list()
+        for k, v in req.items():
+            if v == "":
+                missing.append(k)
+        if missing:
+            feedback = f"Missing fields for {', '.join(missing)}"
+            return render_template("public/sign_up.html", feedback = feedback)
+        return redirect(request.url)
+
+    return render_template("public/sign_up.html")
+
+@app.route("/profile/<username>")
+def profile(username):
+  users = {
+    "mitsuhiko": {
+        "name": "Armin Ronacher",
+        "bio": "Creatof of the Flask framework",
+        "twitter_handle": "@mitsuhiko"
+    },
+    "gvanrossum": {
+        "name": "Guido Van Rossum",
+        "bio": "Creator of the Python programming language",
+        "twitter_handle": "@gvanrossum"
+    },
+    "elonmusk": {
+        "name": "Elon Musk",
+        "bio": "technology entrepreneur, investor, and engineer",
+        "twitter_handle": "@elonmusk"
+    }
+  }  
+
+  user = None
+  if username in users:
+        user = users[username]
+
+  return render_template("public/profile.html", username = username, user = user)
